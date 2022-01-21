f0ad84b1e93cd2d3d17cb8422b36fdf169bce511
Angel Hernandez <angelhernandez2021@u.northwestern.edu>
2022-01-21 12:27:07 -0600

    framework for bluetooth

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,51 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+// constants
+const appBarTitle = 'Software Internship Exercise';
 const textMsg = 'Hello Zenblen!';
 const textColor = Color(0xfff3f6f4);
 const backgroundColor = Color(0xff318ce7);
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Center(
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  // this function should send the bluetooth command
+  void incrementCounter() {
+    setState(() {
+      // send bluetooth command here
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Software Internship Exercise'),
+      ),
+      body: Center(
         child: ElevatedButton(
           style: ButtonStyle(
             backgroundColor: MaterialStateProperty.all<Color>(backgroundColor),
           ),
-          onPressed: () {},
+          onPressed: () => {incrementCounter()},
           child: Text(
             textMsg,
             style: TextStyle(color: textColor),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
